74f193d9d7c2e53810b6cdf8227176f11052f3bd
Murilo <murilofidelis2012@gmail.com>
2018-02-04 16:05:34 -0200

    web config

--- studo-service/src/main/java/br/com/studo/domain/Professor.java
+++ studo-service/src/main/java/br/com/studo/domain/Professor.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -47,9 +48,11 @@ public class Professor implements Serializable {
 
     @NotNull
     @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "codigo_email")
     private Email email;
 
     @NotNull
     @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "codigo_endereco")
     private Endereco endereco;
 }
--- studo-service/src/main/java/br/com/studo/web/resource/TurmaResource.java
+++ studo-service/src/main/java/br/com/studo/web/resource/TurmaResource.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin
 @RestController
 @RequestMapping("turmas")
 public class TurmaResource {
