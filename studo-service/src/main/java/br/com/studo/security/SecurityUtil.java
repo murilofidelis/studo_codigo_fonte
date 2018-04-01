9590711a5112d42b7c603f7bef2e079fa5cb941e
Murilo <murilofidelis2012@gmail.com>
2018-04-01 12:41:46 -0300

    Security util

---
+++ studo-service/src/main/java/br/com/studo/security/SecurityUtil.java
@@ -0,0 +1,25 @@
+package br.com.studo.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Objects;
+
+public final class SecurityUtil {
+
+    private SecurityUtil() {
+    }
+
+    /* Métdo responsável por retornar o login do usuário logado */
+
+    public static String getCurrentUserLogin() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        String login = null;
+        if (Objects.nonNull(authentication)) {
+            login = (String) authentication.getPrincipal();
+        }
+        return login;
+    }
+}
